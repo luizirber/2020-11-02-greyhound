37bf866ff578ea3b832e2bdee6eb0147c3250659
Luiz Irber <luiz.irber@gmail.com>
2020-11-14 15:35:40 -0800

    start yew frontend

---
+++ server/frontend/src/context.rs
@@ -0,0 +1,61 @@
+use std::time::Duration;
+
+use log::info;
+use serde::{Deserialize, Serialize};
+use yew::services::interval::IntervalService;
+use yew::services::Task;
+use yew::worker::*;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Request {
+    GetDataFromServer,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Response {
+    DataFetched,
+}
+
+pub enum Msg {
+    Updating,
+}
+
+pub struct Worker {
+    link: AgentLink<Worker>,
+    _task: Box<dyn Task>,
+}
+
+impl Agent for Worker {
+    type Reach = Context<Self>;
+    type Message = Msg;
+    type Input = Request;
+    type Output = Response;
+
+    fn create(link: AgentLink<Self>) -> Self {
+        let duration = Duration::from_secs(3);
+        let callback = link.callback(|_| Msg::Updating);
+        let task = IntervalService::spawn(duration, callback);
+        Worker {
+            link,
+            _task: Box::new(task),
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) {
+        match msg {
+            Msg::Updating => {
+                info!("Tick...");
+            }
+        }
+    }
+
+    fn handle_input(&mut self, msg: Self::Input, who: HandlerId) {
+        info!("Request: {:?}", msg);
+        match msg {
+            Request::GetDataFromServer => {
+                // TODO fetch actual data
+                self.link.respond(who, Response::DataFetched);
+            }
+        }
+    }
+}
---
+++ server/frontend/src/job.rs
@@ -0,0 +1,72 @@
+use std::time::Duration;
+
+use log::info;
+use serde::{Deserialize, Serialize};
+use yew::services::interval::IntervalService;
+use yew::services::Task;
+use yew::worker::*;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Request {
+    GetDataFromServer,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Response {
+    DataFetched,
+}
+
+pub enum Msg {
+    Initialized,
+    Updating,
+    DataFetched,
+}
+
+pub struct Worker {
+    link: AgentLink<Worker>,
+    _task: Box<dyn Task>,
+}
+
+impl Agent for Worker {
+    type Reach = Job<Self>;
+    type Message = Msg;
+    type Input = Request;
+    type Output = Response;
+
+    fn create(link: AgentLink<Self>) -> Self {
+        let duration = Duration::from_secs(3);
+        let callback = link.callback(|_| Msg::Updating);
+        let task = IntervalService::spawn(duration, callback);
+
+        link.send_message(Msg::Initialized);
+        Worker {
+            link,
+            _task: Box::new(task),
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) {
+        match msg {
+            Msg::Initialized => {
+                info!("Initialized!");
+            }
+            Msg::Updating => {
+                info!("Tick...");
+            }
+            Msg::DataFetched => {
+                info!("Data was fetched");
+            }
+        }
+    }
+
+    fn handle_input(&mut self, msg: Self::Input, who: HandlerId) {
+        info!("Request: {:?}", msg);
+        match msg {
+            Request::GetDataFromServer => {
+                // TODO fetch actual data
+                self.link.respond(who, Response::DataFetched);
+                self.link.send_message(Msg::DataFetched);
+            }
+        }
+    }
+}
